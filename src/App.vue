99465180957961b943f0aa2f13055e6a58e46a2d
mistadane <samrehonline03@gmail.com>
2020-08-27 21:41:28 -0500

    shortener challenge

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  font-size: 18px
+}
+</style>
